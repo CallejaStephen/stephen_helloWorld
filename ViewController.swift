8a495417b9c6f2aefd5cecccea0c77170fdde6a5
CallejaStephen <stephen.calleja@gmail.com>
2016-10-19 09:11:05 +0200

    Add files via upload
    Hello world 1

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stephen_HelloWorld
+//
+//  Created by maint on 10/12/16.
+//  Copyright © 2016 maint. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
